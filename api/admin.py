f1cf54c326760ada46a7a5ff8df55eba898ed1fd
KaiBa Fou <foueducation@gmail.com>
2022-04-27 15:39:08 +0700

    modify list_display for admin panel

--- api/admin.py
+++ api/admin.py
@@ -4,6 +4,16 @@ from .models.books import Book
 from .models.movies import Movie
 # Register your models here.
 
-admin.site.register(CustomUser)
-admin.site.register(Book)
-admin.site.register(Movie)
+class UserAdminConfig(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email')
+
+class BookAdminConfig(admin.ModelAdmin):
+    list_display = ('id', 'isbn', 'title', 'year')
+
+class MovieAdminConfig(admin.ModelAdmin):
+    list_display = ('id', 'title', 'year')
+
+
+admin.site.register(CustomUser, UserAdminConfig)
+admin.site.register(Book, BookAdminConfig)
+admin.site.register(Movie, MovieAdminConfig)
